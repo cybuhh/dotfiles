82ab36c7b3743e1e541bd5cceb5235db9585ded0
cybuhh <mateusz.cyburt@gmail.com>
2016-01-29 00:28:03 +0100

    added some export variables

--- common.sh
+++ common.sh
@@ -1,5 +1,10 @@
-DOTFILES_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-DOTFILES_DATA_PATH="$DOTFILES_PATH/data"
+#!/usr/bin/env bash
+export LC_ALL=C
+export LC_CTYPE=en_US.UTF-8
+export EDITOR=vim
+export VISUAL=vim
+export DOTFILES_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+export DOTFILES_DATA_PATH="$DOTFILES_PATH/data"
 
 test -d $DOTFILES_DATA_PATH || mkdir $DOTFILES_DATA_PATH
 
