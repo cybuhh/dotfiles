ec5f46c7e81a809911b4dae14d80a3da9cfe80db
cybuhh <mateusz.cyburt@gmail.com>
2016-01-28 22:17:58 +0100

    net/whatportis-update added

--- net.sh
+++ net.sh
@@ -1,9 +1,14 @@
-test -f "$DOTFILES_DATA_PATH/services.csv" || curl -s http://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.csv | cut -d ',' -f 1,2,3,4 > "$DOTFILES_DATA_PATH/services.csv"
+whatportisUpdate() {
+	curl -s http://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.csv | cut -d ',' -f 1,2,3,4 > "$DOTFILES_DATA_PATH/services.csv"
+}
 
 getWhatportis() {
 	grep -e 'Service Name' -e "^$1," -e ",$1," $DOTFILES_DATA_PATH/services.csv | column -s , -t
 }
 
+test -f "$DOTFILES_DATA_PATH/services.csv" || getWhatportisUpdate
+
 alias whatismyip='curl ipinfo.io/ip'
 alias whatismyip-local='ifconfig | grep "inet " | grep -v 127.0.0.1 | cut -d " " -f 2'
 alias whatportis=getWhatportis
+alias whatportis-update=whatportisUpdate
