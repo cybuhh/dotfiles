07372a1be18cfe2fed01c66298bb2a7eadba7143
cybuhh <mateusz.cyburt@gmail.com>
2016-01-28 22:17:35 +0100

    added creating missing data folder

--- common.sh
+++ common.sh
@@ -1,4 +1,8 @@
 DOTFILES_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+DOTFILES_DATA_PATH="$DOTFILES_PATH/data"
+
+test -d $DOTFILES_DATA_PATH || mkdir $DOTFILES_DATA_PATH
+
 alias dotfiles-commit='cd $DOTFILES_PATH && (echo -e "Please enter commit message: \c"; read MSG ; git commit -m "$MSG") ; cd - > /dev/null'
 alias dotfiles-push='cd $DOTFILES_PATH; git push'
 
