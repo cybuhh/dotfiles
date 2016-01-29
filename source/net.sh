6bada786bb433250a758b32bcb87638f54c76d3b
cybuhh <mateusz.cyburt@gmail.com>
2016-01-29 12:26:10 +0100

    added liquidprompt submodule

--- source/net.sh
+++ source/net.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
-whatportisUpdate() {
-	curl -s http://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.csv | cut -d ',' -f 1,2,3,4 > "$DOTFILES_DATA_PATH/services.csv"
-}
+servicesFile=$DOTFILES_VENDOR_PATH/services/service-names-port-numbers.csv
 
 getWhatportis() {
-	grep -e 'Service Name' -e "^$1," -e ",$1," $DOTFILES_DATA_PATH/services.csv | column -s , -t
+
+    if [ "$1" == 'update' ]; then
+        curl -s http://www.iana.org/assignments/service-names-port-numbers/$servicesFile | cut -d ',' -f 1,2,3,4 > "$servicesFile"
+	    needle=$2
+	else
+	    needle=$1
+	fi
+
+	if [ -n "$needle" ]; then
+    	grep -e 'Service Name' -e "^$needle," -e ",$needle," "$servicesFile" | column -s , -t
+    fi
 }
 
-test -f "$DOTFILES_DATA_PATH/services.csv" || getWhatportisUpdate
+test -f "$servicesFile" || getWhatportis update
 
 alias curl-size-raw="curl -s --write-out \"%{size_download}\n\" --output /dev/null"
 alias curl-size-gzip="curl -s -H 'Accept-Encoding: gzip,deflate' --write-out \"%{size_download}\n\" --output /dev/null"
