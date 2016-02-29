197c7fc2e2cc133220930f94c70c92909cefeb4d
Mateusz Cyburt <mateusz.cyburt@gmail.com>
2016-02-29 08:08:59 +0100

    updated kali-ip alias

--- source/work.sh
+++ source/work.sh
@@ -1,1 +1,1 @@
-alias kali-ip="ssh 192.168.10.21 \"sudo cat /var/log/messages | grep iptables-ping | tail -n 1 | cut -d ' ' -f 12 | sed 's/SRC=//'\""
+alias kali-ip="ssh 192.168.10.21 \"sudo cat /var/log/messages | grep iptables-ping | tail -n 1 | cut -d ' ' -f 11 | sed 's/SRC=//'\""
