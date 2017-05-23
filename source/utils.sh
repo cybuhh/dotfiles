0dd1ffd2d8b3554544b88df3b7e166e86a57b653
cybuhh <mateusz.cyburt@gmail.com>
2017-05-23 07:41:59 +0200

    improved napi-find response

--- source/utils.sh
+++ source/utils.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 alias ll='ls -al'
-alias napi-find='find . -name "*.avi" -o -name "*.mpg" -o -name "*.mpeg" -o -name "*.mp4" -o -name "*.qt" -o -name "*.mkv" -o -name "*.m2v" | while read file; do test -f "${file%.*}.txt" || qnapi "$file"; done'
+alias napi-find='find . -name "*.avi" -o -name "*.mpg" -o -name "*.mpeg" -o -name "*.mp4" -o -name "*.qt" -o -name "*.mkv" -o -name "*.m2v" | while read file; do test -f "${file%.*}.txt" || (qnapi "$file" | grep "Dopasowywanie" > /dev/null && echo "Downloaded subtitles for $file"); done'
