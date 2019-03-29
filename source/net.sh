0c7e61fcb011a1c28fb5a4ec7240286e3c1811b2
cybuhh <>
2019-03-29 13:39:53 +0100

    add net::wifi-signal

--- source/net.sh
+++ source/net.sh
@@ -59,3 +59,5 @@ function geoip() {
 }
 
 alias geoip-dns='geoip $(whatismydns)'
+
+alias wifi-signal='clear; while x=1; do /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | grep CtlRSSI | sed -e 's/^.*://g' | xargs -I SIGNAL printf "\rRSSI dBm: SIGNAL"; sleep 0.5; done'
