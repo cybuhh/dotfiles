181e7dcd3ede701733b7696063e0241c2cc0489c
cybuhh <>
2019-08-08 11:41:13 +0200

    add util::excuse

--- source/util.sh
+++ source/util.sh
@@ -18,7 +18,7 @@ alias m4a2mp3='for i in *.m4a; do ffmpeg -i "$i" "${i}".mp3; done'
 alias heic2jpg='for i in *.heic; do magick "$i" "${i/.heic/.jpg}"; done'
 alias yt2mp3='youtube-dl -x --audio-format mp3'
 function file-lowercase {
-	find . -name "$1" -print0 | 
+	find . -name "$1" -print0 |
     while IFS= read -r -d $'\0' oldName; do
 		echo "$oldName"
 		#newName=$(echo "$oldName" | tr '[:upper:]' '[:lower:]')
@@ -31,3 +31,5 @@ function file-lowercase {
 function count-uniq {
   sort | uniq -c | sort -r | head -n "${1:-100}"
 }
+
+alias excuse="bash $DOTFILES_VENDOR_PATH/excuse/bin/excuse.sh"
