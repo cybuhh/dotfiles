863321d031bb3edaf21d52a6270cf34f677c47b7
cybuhh <>
2019-10-29 10:59:50 +0100

    add apps::sublimemerge

--- source/apps.sh
+++ source/apps.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # shellcheck disable=SC1117
+alias sublimemerge="/Applications/Sublime\ Merge.app/Contents/MacOS/sublime_merge"
 sublimetext() { "open -a Sublime\ Text $1"; }
 textedit() { "open -a TextEdit $1"; }
 alias chrome-dws="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --disable-web-security --user-data-dir=$HOME/Library/Application\ Support/Google/Chrome/Default"
