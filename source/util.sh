e27f2f5f009618476687281c46c047f0b61a4094
cybuhh <>
2020-03-30 20:03:56 +0200

    update qnapi command

--- source/util.sh
+++ source/util.sh
@@ -13,7 +13,7 @@ alias cd-workspace-svp='cd ~/Workspace/svp'
 alias grep='grep --color=auto'
 
 alias ll='ls -al'
-alias napi-find='find . -name "*.avi" -o -name "*.mpg" -o -name "*.mpeg" -o -name "*.mp4" -o -name "*.qt" -o -name "*.mkv" -o -name "*.m2v" | while read file; do test -f "${file%.*}.txt" || (qnapi -q "$file" | grep "Dopasowywanie" > /dev/null && echo "Downloaded subtitles for $file"); done'
+alias napi-find='find . -name "*.avi" -o -name "*.mpg" -o -name "*.mpeg" -o -name "*.mp4" -o -name "*.qt" -o -name "*.mkv" -o -name "*.m2v" | while read file; do test -f "${file%.*}.srt" || (qnapi -q "$file" | grep "Dopasowywanie" > /dev/null && echo "Downloaded subtitles for $file"); done'
 alias m4a2mp3='for i in *.m4a; do ffmpeg -i "$i" "${i}".mp3; done'
 alias heic2jpg='for i in `ls -1 | grep -i ''.heic'' | tr ''.HEIC'' ''.heic''`; do magick "$i" "${i/.heic/.jpg}"; done'
 alias yt2mp3='youtube-dl -x --audio-format mp3'
