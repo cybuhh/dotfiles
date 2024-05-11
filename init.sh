c2d6ef8531eab8ee42d32e2f07aaf72b7ef86ea0
cybuhh <cybuhh@users.noreply.github.com>
2024-05-11 22:33:04 +0200

    add npm completion

--- init.sh
+++ init.sh
@@ -144,6 +144,7 @@ export NVM_DIR="$HOME/.nvm"
 if [ -d $NVM_DIR ]; then
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  source <(npm completion)
 fi
 
 export RVM_DIR="$HOME/.rvm"
